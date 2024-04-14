ba0a1a6b3cdaee07b6bb87804b591f03e1b11dd0
Tsuyoshi Yoshioka <tsuyogoro@gmail.com>
2024-04-14 10:27:56 +0900

    Load data as a global data

--- app/api/schedule/today/route.ts
+++ app/api/schedule/today/route.ts
@@ -1,6 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
+import { calendarSource } from "../../data/datasource";
 
 export function GET(request: NextRequest): NextResponse {
+    console.log('loading calendar'); 
+    const d = calendarSource;
+    console.log('loaded calendar');
     return NextResponse.json(
         { message: `aaaaaaa` },
         { status: 200 },
