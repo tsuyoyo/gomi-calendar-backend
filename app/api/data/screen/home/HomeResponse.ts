0c97148df06d621bc15f375aa0e368e3ea72e13c
Tsuyoshi Yoshioka <tsuyogoro@gmail.com>
2024-05-25 21:25:18 +0900

    Fix issue

--- app/api/data/screen/home/HomeResponse.ts
+++ app/api/data/screen/home/HomeResponse.ts
@@ -1,4 +1,5 @@
 import { LinkComponent } from '../common/LinkComponent';
+import { HomeAreaDateComponent } from './HomeAreaDateComponent';
 import { HomeComponentType } from './HomeComponentType';
 import { HomeNextComponent } from './HomeNextComponent';
 import { HomeTodayComponent } from './HomeTodayComponent';
@@ -6,6 +7,7 @@ import { HomeTodayComponent } from './HomeTodayComponent';
 export type HomeResponse = {
   layout: { type: HomeComponentType; index: number }[];
 
+  areaDateComponent: HomeAreaDateComponent[];
   todayComponents: HomeTodayComponent[];
   nextComponents: HomeNextComponent[];
   links: LinkComponent[];
