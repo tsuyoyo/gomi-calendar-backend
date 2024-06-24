aefded5880ff66db498733485ef51f176af2d2fb
Tsuyoshi Yoshioka <tsuyogoro@gmail.com>
2024-06-24 09:18:08 +0900

    Return today's reminder

--- app/api/schedule/[id]/reminder/buildReminders.ts
+++ app/api/schedule/[id]/reminder/buildReminders.ts
@@ -7,29 +7,53 @@ import {
 import { getDateInJst } from '../../../date/getDateInJst';
 import { getStringResource } from '../../../resources/strings';
 import { getNextDayForType } from '../next/[type]/getNextDayForType';
+import { getTrashCollectionTypes } from '../today/getTrashCollectionTypes';
 
 export const buildReminders = (
   schedule: CalendarEntry,
   locale: string | null,
   remindDay: RemindDay,
 ): Reminder[] => {
-  const nextDays = Object.values(TrashType).map((type) => {
-    const today = getDateInJst();
-    return {
-      type,
-      nextDay: getNextDayForType(
-        schedule,
+  const today = getDateInJst();
+  const remindDays: {
+    type: TrashType;
+    remindDate: Date;
+  }[] = [];
+
+  if (remindDay === 'day-on-the-day') {
+    const todayCollection = getTrashCollectionTypes(
+      schedule,
+      today.getFullYear(),
+      today.getMonth() + 1,
+      today.getDate(),
+    );
+    todayCollection.forEach((type) => {
+      remindDays.push({
         type,
-        today.getFullYear(),
-        today.getMonth() + 1,
-        today.getDate(),
-      ),
-    };
+        remindDate: today,
+      });
+    });
+  }
+
+  Object.values(TrashType).forEach((type) => {
+    const remindDate = getNextDayForType(
+      schedule,
+      type,
+      today.getFullYear(),
+      today.getMonth() + 1,
+      today.getDate(),
+    );
+    if (remindDate !== null) {
+      remindDays.push({
+        type,
+        remindDate,
+      });
+    }
   });
 
   const reminders = Array<Reminder>();
-  nextDays.forEach(({ type, nextDay }) => {
-    if (nextDay === null) {
+  remindDays.forEach(({ type, remindDate }) => {
+    if (remindDate === null) {
       return;
     }
     const displayTrashType = buildResponseTrashTypeData(
@@ -46,7 +70,7 @@ export const buildReminders = (
           ? `${displayTrashType} is collected today.`
           : `今日は${displayTrashType}の収集日です。`;
 
-    const dateToRemind = nextDay;
+    const dateToRemind = remindDate;
     if (remindDay === 'day-before') {
       dateToRemind.setDate(dateToRemind.getDate() - 1);
     }
